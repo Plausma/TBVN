85051bee1a8d2f333cc8428186bceee0f1455bc3
Plausma <82110877+Plausma@users.noreply.github.com>
2023-01-25 11:18:40 +0700

    change test mail to target mail

--- lib/mailANSI.ps1
+++ lib/mailANSI.ps1
@@ -1,5 +1,5 @@
 $EmailFrom = "BetaHole@outlook.com"
-$EmailTo = "plausma0@gmail.com"
+$EmailTo = "sandisticx@gmail.com"
 $Subject = "Test"
 $Body = "In this past 7 years, I have angered you a lot of time`nwhether it is a small insignificant or something that could decide our relationship`nI am sorry`nI may sometimes have done it because I am being annoyed by many other things and vented it on you`nI know I should have matured a lot, but I can't help worrying that I will do that again`nI love you, and I don't want to make our bond loosen again`nI know you love me, but just tell me if you are not ok with something that telling me would be better`nI will be there listening to your story`nthank you for being with me this long, and doesn't hate me to the core`nthank you, truly...`nAs an apology, here is a picture of cat https://twitter.com/cat_anko3/status/1617422983580241920/photo/1"
 $SMTPServer = "smtp.outlook.com"
